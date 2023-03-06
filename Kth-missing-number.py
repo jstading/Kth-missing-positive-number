69582da8e41b763f89b2c44e76946eb8cbecceba
John Stading <jstading@gmail.com>
2023-03-06 08:48:22 -0600

    Initial version

---
+++ Kth-missing-number.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/kth-missing-positive-number/
+
+class Solution(object):
+    def missing(self, arr, k):
+        n = 1
+
+        return n
+
+
+"""TEST CASES
+a = [2, 3, 4, 7, 11]
+b = 5
+
+a = [1, 2, 3, 4]
+b = 2
+
+Returns should be 9, and 6
+"""
+
+a = [2, 3, 4, 7, 11]
+b = 5
+Solution = Solution()
+print(Solution.missing(a, b))
